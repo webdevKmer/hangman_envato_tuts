61dd8b71bec3a2cffaf23aff7b9326ea20d27592
webdevKmer <webdevKmer@gmail.com>
2022-10-03 03:45:51 +0000

    testing the setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Hello {props.name}</h2>
+      <p>Do you want to play the HangMan game ?</p>
+    </>
   );
 }
 
